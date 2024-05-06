709026d911f244cda618c2b0967d9870f50129cf
Wisnu Imam <90234571+wisnu2907@users.noreply.github.com>
2024-05-06 19:45:02 +0700

    Update agv_db_drive_controller_2_id11_AWG.ino

--- Arduino/agv_db_drive_controller_2_id11_AWG/agv_db_drive_controller_2_id11_AWG.ino
+++ Arduino/agv_db_drive_controller_2_id11_AWG/agv_db_drive_controller_2_id11_AWG.ino
@@ -1,9 +1,11 @@
-// rodikwahyuindrawan@gmail.com
-// @awgrobotic2023
+// wisnu.imam.satrio@gmail.com
+// @KRTMI2024
 
-#define pulse_rotation 1988 //
-#define d_wheel 15 // cm
+#define pulse_rotation 1988  //
+#define d_wheel 15           // cm
 #define k_wheel (3.14 * 15)
+#define spd 8
+#define spd2 5
 
 /*
    range speed -50 0 50
@@ -16,32 +18,27 @@
 */
 //-----------------------------------------
 
-#include <ModbusRtu.h>
-
-// data array for modbus network sharing
-uint16_t data_comm[6] = { 0, 0, 0, 0, 0, 0 };
-
-Modbus slave(11, Serial, 0); // this is slave @10 and RS-232 or USB-FTDI
-
 //-----------------------------------------
 
 #include <Encoder.h>
 
+#define ledbuiltin 13
+
 Encoder myEncL(3, 4);
 Encoder myEncR(2, 8);
 
-long oldPositionL  = -999;
-long oldPositionR  = -999;
+long oldPositionL = -999;
+long oldPositionR = -999;
 
-void read_encoder()
-{
+void read_encoder() {
   long newPositionL = myEncL.read();
   long newPositionR = myEncR.read();
   if (newPositionL != oldPositionL || newPositionR != oldPositionR) {
     oldPositionL = newPositionL;
     oldPositionR = newPositionR;
-    Serial.print(newPositionL); Serial.print('\t');
-    Serial.println(newPositionR); // Serial.print('\t');
+    Serial.print(newPositionL);
+    Serial.print('\t');
+    Serial.println(newPositionR);  // Serial.print('\t');
   }
 }
 
@@ -55,13 +52,12 @@ const unsigned int PWM_R = 10;
 float mErrL = 0, mErrR = 0;
 float mdErrL = 0, mdErrR = 0;
 float SpeedL = 0, SpeedR = 0;
-float mLastL = 0,  mLastR = 0;
+float mLastL = 0, mLastR = 0;
 int pwmL = 0, pwmR = 0;
 
 float CountL = 0, CountR = 0;
 
-void init_motor()
-{
+void init_motor() {
   pinMode(DIR_L, OUTPUT);
   pinMode(PWM_L, OUTPUT);
 
@@ -69,71 +65,49 @@ void init_motor()
   pinMode(PWM_R, OUTPUT);
 }
 
-void agv_motor(int ll, int rr)
-{
-  if (data_comm[0] == 1)
-  {
-    if (ll < 0)
-    {
-      digitalWrite(DIR_L, LOW);
-      analogWrite(PWM_L, -ll);
-    }
-    else if (ll > 0)
-    {
-      digitalWrite(DIR_L, HIGH);
-      analogWrite(PWM_L, ll);
-    }
-    else
-    {
-      analogWrite(PWM_L, 0);
-    }
-
-    if (rr < 0)
-    {
-      digitalWrite(DIR_R, HIGH);
-      analogWrite(PWM_R, -rr);
-    }
-    else if (rr > 0)
-    {
-      digitalWrite(DIR_R, LOW);
-      analogWrite(PWM_R, rr);
-    }
-    else
-    {
-      analogWrite(PWM_R, 0);
-    }
-  }
-  else
-  {
+void agv_motor(int ll, int rr) {
+  digitalWrite(13, HIGH);
+  if (ll < 0) {
+    digitalWrite(DIR_L, LOW);
+    analogWrite(PWM_L, -ll);
+  } else if (ll > 0) {
+    digitalWrite(DIR_L, HIGH);
+    analogWrite(PWM_L, ll);
+  } else {
     analogWrite(PWM_L, 0);
-    analogWrite(PWM_R, 0);
-    mErrL = 0; mErrR = 0;
-    mdErrL = 0; mdErrR = 0;
-    SpeedL = 0; SpeedR = 0;
-    mLastL = 0;  mLastR = 0;
   }
 
+  if (rr < 0) {
+    digitalWrite(DIR_R, HIGH);
+    analogWrite(PWM_R, -rr);
+  } else if (rr > 0) {
+    digitalWrite(DIR_R, LOW);
+    analogWrite(PWM_R, rr);
+  } else {
+    analogWrite(PWM_R, 0);
+  }
 }
 
 
-void com_agv_motor(int dSL, int dSR)
-{
+void com_agv_motor(int dSL, int dSR) {
   int dRL = 0, dRR = 0;
   delay(13);
   dRL = abs(myEncL.read());
   dRR = abs(myEncR.read());
 
-  CountL += (float)dRL / 42.2;
-  CountR += (float)dRR / 42.2;
+  CountL += (float)dRL / 49.852;
+  CountR += (float)dRR / 49.852;
 
-  data_comm[4] = CountL;
-  data_comm[5] = CountR;
+  //42.2 = pulse_rotation/(k_wheel)
+
+  // Serial.print(CountL);
+  // Serial.println(CountR);
 
   myEncL.write(0);
   myEncR.write(0);
 
-  mErrL =  abs(dSL) - dRL;
-  mErrR =  abs(dSR) - dRR;
+  mErrL = abs(dSL) - dRL;
+  mErrR = abs(dSR) - dRR;
 
   mdErrL = mErrL - mLastL;
   mLastL = mErrL;
@@ -142,98 +116,135 @@ void com_agv_motor(int dSL, int dSR)
   mdErrR = mErrR - mLastR;
   mLastR = mErrR;
 
-  SpeedL += (float)(mErrL * 0.3) +  (float)(mdErrL * 0.4 );
-  SpeedR += (float)(mErrR * 0.3) +  (float)(mdErrR * 0.4 );
+  SpeedL += (float)(mErrL * 0.3) + (float)(mdErrL * 0.4);
+  SpeedR += (float)(mErrR * 0.3) + (float)(mdErrR * 0.4);
 
   if (SpeedL >= 255) SpeedL = 255;
   if (SpeedR >= 255) SpeedR = 255;
 
   if (dSL < 0) pwmL = -SpeedL;
   else if (dSL > 0) pwmL = SpeedL;
-  else        pwmL = 0;
+  else pwmL = 0;
   if (dSR < 0) pwmR = -SpeedR;
   else if (dSR > 0) pwmR = SpeedR;
-  else        pwmR = 0;
+  else pwmR = 0;
 
   agv_motor(pwmL, pwmR);
 }
 
+void Backward() {
+  com_agv_motor(-spd, -spd);
+}
+
+void Forward() {
+  com_agv_motor(spd, spd);
+}
+void Backwardgt() {
+  com_agv_motor(-spd2, -spd2);
+}
+
+void Forwardgt() {
+  com_agv_motor(spd2, spd2);
+}
+
+void SlideL() {
+  com_agv_motor(-11, 11);
+}
+
+void Stop() {
+  com_agv_motor(0, 0);
+  analogWrite(PWM_L, 0);
+  analogWrite(PWM_R, 0);
+  mErrL = 0;
+  mErrR = 0;
+  mdErrL = 0;
+  mdErrR = 0;
+  SpeedL = 0;
+  SpeedR = 0;
+  mLastL = 0;
+  mLastR = 0;
+  delay(13);
+}
 
 #include <Arduino_FreeRTOS.h>
 
+
 // define two tasks for Blink & AnalogRead
-void TaskComm( void *pvParameters );
-void TaskMotor( void *pvParameters );
+void TaskComm(void *pvParameters);
+void TaskMotor(void *pvParameters);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  slave.start();
+  Serial.setTimeout(1);
   init_motor();
+  pinMode(13, OUTPUT);
+
+
 
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+    ;
   }
 
-  // Now set up two tasks to run independently.
   xTaskCreate(
-    TaskComm
-    ,  "Comm"   // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL );
+    TaskComm, "Comm"  // A name just for humans
+    ,
+    128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,
+    NULL, 2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,
+    NULL);
 
   xTaskCreate(
-    TaskMotor
-    ,  "Motor"
-    ,  128  // Stack size
-    ,  NULL
-    ,  1  // Priority
-    ,  NULL );
+    TaskMotor, "Motor", 128  // Stack size
+    ,
+    NULL, 1  // Priority
+    ,
+    NULL);
 
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
-void loop()
-{
-  // Empty. Things are done in Tasks.
+
+void loop() {
 }
 
-/*--------------------------------------------------*/
-/*---------------------- Tasks ---------------------*/
-/*--------------------------------------------------*/
+String input;
 
-void TaskComm(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
+void TaskComm(void *pvParameters) {
+  (void)pvParameters;
 
-  for (;;) // A Task shall never return or exit.
-  {
+  for (;;) {
+    if (Serial.available() > 0) {
+      input = Serial.readStringUntil('\n');
+      CountL = 0, CountR = 0;
+    }
 
-    slave.poll( data_comm, 6 );
-
-    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+    vTaskDelay(1);
   }
 }
 
 void TaskMotor(void *pvParameters)  // This is a task.
 {
-  (void) pvParameters;
+  (void)pvParameters;
 
-  for (;;)
-  {
-    if (data_comm[3] == 1) {
-      CountL = 0;
-      CountR = 0;
-      data_comm[3] = 0;
-      data_comm[4] = 0;
-      data_comm[5] = 0;
-    }
-
-    com_agv_motor(data_comm[1], data_comm[2]);
+  for (;;) {
+    if (input == "1") {
+      Forward();
+    } else if (input == "2") {
+      Backward();
+    } else if (input == "0") {
+      Stop();
+    } else if (input == "3") {
+      SlideL();
+      if (CountL >= 100 || CountR >= 100) {
+        Stop();
+      }
+    } else if (input == "4") {
+      Forwardgt();
+    } else if (input == "5") {
+      Backwardgt();
+    } else Stop();
 
     vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
